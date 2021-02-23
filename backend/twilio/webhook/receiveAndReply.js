44bcccf4938030e402a2f934f158f47768e7fefe
Oscar <oscarperezlomes@gmail.com>
2021-02-23 16:31:03 -0600

    Se actualiza WebHook WhatsApp

--- backend/twilio/webhook/receiveAndReply.js
+++ backend/twilio/webhook/receiveAndReply.js
@@ -15,7 +15,7 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
-
+  
   twiml.message('The Robots are coming! Head for the hills!');
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
