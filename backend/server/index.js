5ef7487d1257d38c1a5bd15ce7a09e735ea3870a
Oscar <oscarperezlomes@gmail.com>
2021-02-23 16:16:39 -0600

    Se implementa WebHook para recibir y enviar WhatsApp

--- backend/server/index.js
+++ backend/server/index.js
@@ -10,7 +10,8 @@ Actualizaciones:
 
 
 //=================== Settings =======================
-
+const http = require('http');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const express = require('express');
 const server = express();
 const bcrypt = require('bcrypt');
@@ -143,6 +144,20 @@ server.post('/sesionWhatsApp', async (req, res) => {
 
 });
 
+//------------------ WebHook WhatsApp --------------------
+
+server.post('/conversationWhatsApp', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  twiml.message('The Robots are coming! Head for the hills!');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+// http.createServer(server).listen(1337, () => {
+//   console.log('Express server listening on port 1337');
+// });
 
 //=================== Static Files ============================
 
