5b013638691528d652910b77fa1050394a244918
Oscar <oscarperezlomes@gmail.com>
2021-02-23 13:48:50 -0600

    Se configuran variables para autenticacion a Twilio

--- backend/twilio/send-message-whatsapp.js
+++ backend/twilio/send-message-whatsapp.js
@@ -7,44 +7,22 @@ Actualizaciones:
 * 09/02/2021 ::: Se crea funcion sendMessageWhatsApp() para el envio de mensajes por WhatsApp via Twilio
 */
 
-const config = require('../config');
 
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } = require('../config');
 
 async function sendMessageWhatsApp(body) {
 
   try {
-
-    /*const accountSid = 'AC4fa52c00d1c5559bff85e2b8851d8fee'; 
-    const authToken = '9a3723dc618b353c13a63edeae4f34f3';
-
-    const client = require('twilio')(accountSid, authToken);
-                          
-    client.conversations.conversations('CHxxxx')
-          .webhooks
-          .create({
-                    //'configuration.url': 'https://timberwolf-mastiff-9776.twil.io/demo-reply',
-                    'configuration.url': 'https://demo.twilio.com/welcome/sms/reply/',
-                    'configuration.method': 'POST',
-                    'configuration.filters': ['onMessageAdded'],
-                    target: 'webhook'
-          })
-          .then(webhook => console.log(webhook.sid));*/
     
-    const accountSid = 'AC4fa52c00d1c5559bff85e2b8851d8fee'; 
-    const authToken = '9a3723dc618b353c13a63edeae4f34f3'; 
-          
-    const client = require('twilio')(accountSid, authToken);  https://demo.twilio.com/welcome/sms/reply/
-           
+    const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+     
     client.messages
           .create({   
             to: `whatsapp:+521${body.telefono}`,
             from: 'whatsapp:+14155238886', 
             body: body.mensaje,    
-            //mediaUrl: "https://demo.twilio.com/owl.png"
             mediaUrl: body.mediaUrl
           }) 
-          // .then(message => console.log(message.sid))
           .then(message => console.log(message))
           .done();
 
